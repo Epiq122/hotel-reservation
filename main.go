bfcbbb02911c928dccc3062302642973e2bf28f7
Robert Gleason <l2ob122@gmail.com>
2024-03-21 21:10:56 -0700

    Add .gitignore, Makefile, user_handler.go, hotel-reservation, and types/user.go

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/epiq122/hotel-reservation/api"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
+	listenAddr := flag.String("listen-addr", ":3000", "server listen address")
+	flag.Parse()
+
 	app := fiber.New()
-	app.Get("/foo", handleFoo)
-	app.Listen(":3000")
+	apiv1 := app.Group("/api/v1")
 
-}
+	apiv1.Get("/user", api.HandleGetUsers)
+	apiv1.Get("/user/:id", api.HandleGetUser)
 
-func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Hello, World!",
-	})
+	app.Listen(*listenAddr)
+
 }
